a0fefc3539b5957da4fc7d26d7b899645106ebf9
Jugwang <hjjk0721@gmail.com>
2024-11-18 10:54:07 +0900

    feat: [main] eslint and prettier settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -14,15 +14,17 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react: eslintPluginReact,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
+      semi: ['error', 'never'],
+      indent: ['error', 2],
+      ...eslintPluginReact.configs.flat.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
     },
-  },
-)
+  }
+);
