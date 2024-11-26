37e33727240c94d174019cc325a7ed350bea858b
kimdayeon37 <kdy37912@naver.com>
2024-11-26 16:07:25 +0900

    feat: dangerouselySetInnerHTML 적용

--- src/components/Review/ReactEditor.tsx
+++ src/components/Review/ReactEditor.tsx
@@ -3,10 +3,13 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { CustomQuillEditorView } from './ReactEditor.style';
 import ReactModule from './ReactModule';
+import dompurify from 'dompurify';
 
 const ReactEditor = () => {
   const [content, setContent] = useState<string>('');
-
+  // 스크립트를 활용하여 javascript와 HTML로 악성 코드를 웹 브라우저에 심어,
+  // 사용자 접속시 그 악성코드가 실행되는 것을 XSS, 보안을 위해 sanitize 추가
+  const sanitizer = dompurify.sanitize;
   const formats: string[] = [
     'header',
     'size',
@@ -43,6 +46,10 @@ const ReactEditor = () => {
         <ReactModule />
       </div>
       <ReactQuill theme="snow" modules={modules} formats={formats} id="quillContent" value={content} onChange={setContent} />
+
+      <div id="content">
+        <div dangerouslySetInnerHTML={{ __html: sanitizer(`${content}`) }} />
+      </div>
     </CustomQuillEditorView>
   );
 };
