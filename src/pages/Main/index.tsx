ff6dc218b77cecd87a18a3edc77cd4fa1a6f1091
Jugwang <hjjk0721@gmail.com>
2024-11-27 20:05:54 +0900

    feat: Test for load balancing works

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,19 @@
+import axios from 'axios';
+
 const index = () => {
-  return <div>github action test trial fifth</div>;
+  const fetchTest = async () => {
+    const response = await axios.get('http://pov-elb-01-1418143664.ap-northeast-2.elb.amazonaws.com/actuator/health');
+    console.log(response);
+  };
+  return (
+    <button
+      onClick={() => {
+        fetchTest();
+      }}
+    >
+      연결확인용
+    </button>
+  );
 };
 
 export default index;
