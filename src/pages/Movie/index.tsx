530b1bc708949ef7df002de4ad625c4fab87bd58
Jugwang <hjjk0721@gmail.com>
2024-11-27 20:05:02 +0900

    feat: work start

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,9 +1,23 @@
 import Padded from '../../components/templates/Padded/Padded';
+import axios from 'axios';
+import { Container } from './Movie.styles';
 
 const index = () => {
+  const fetchTest = async () => {
+    const response = await axios.get('http://pov-elb-01-1418143664.ap-northeast-2.elb.amazonaws.com/actuator/health');
+    console.log(response);
+  };
   return (
     <Padded>
-      <div>movie</div>
+      <Container>
+        <button
+          onClick={() => {
+            fetchTest();
+          }}
+        >
+          123123
+        </button>
+      </Container>
     </Padded>
   );
 };
