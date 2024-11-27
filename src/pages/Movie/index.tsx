49c20fbef669d5d518d83744e9d6aa55f26abdec
Jugwang <hjjk0721@gmail.com>
2024-11-27 20:10:18 +0900

    feat: Test for load balancing works wip

---
+++ src/pages/Movie/index.tsx
@@ -0,0 +1,25 @@
+import Padded from '../../components/templates/Padded/Padded';
+import axios from 'axios';
+import { Container } from './Movie.styles';
+
+const index = () => {
+  const fetchTest = async () => {
+    const response = await axios.get('https://pov-elb-01-1418143664.ap-northeast-2.elb.amazonaws.com/actuator/health');
+    console.log(response);
+  };
+  return (
+    <Padded>
+      <Container>
+        <button
+          onClick={() => {
+            fetchTest();
+          }}
+        >
+          123123
+        </button>
+      </Container>
+    </Padded>
+  );
+};
+
+export default index;
