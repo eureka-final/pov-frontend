32a98e235a6836750ece1ffa8c4f64fb5df8e0ef
shinhm1 <shinhm1@naver.com>
2024-12-02 02:01:39 +0900

    feat: Router, 디바이스 너비 대응 useHeaderRender Hook 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css';
 import AppScreen from './routes/AppScreen';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return <AppScreen />;
+  return (
+    <BrowserRouter>
+      <AppScreen />
+    </BrowserRouter>
+  );
 }
 
 export default App;
