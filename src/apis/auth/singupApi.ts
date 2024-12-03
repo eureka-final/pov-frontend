46257ce0ff3a3e678acba968842a8673cca957b2
shinhm1 <shinhm1@naver.com>
2024-12-04 01:43:16 +0900

    fix: signupApi 에러 코드별 에러 메시지 return 추가

--- src/apis/auth/singupApi.ts
+++ src/apis/auth/singupApi.ts
@@ -1,13 +1,24 @@
 import { instance } from '../axiosInstance';
 import type { User } from '../../types/user';
+import axios from 'axios';
 
 /* 회원가입 요청 API */
 export const postAuthSignUpApi = async (user: User) => {
   try {
     const response = await instance.post('/api/auth/signup', user);
-    console.log(response);
     return response.data;
   } catch (error) {
-    console.log('error occured during get auth sign in :', error);
+    if (axios.isAxiosError(error)) {
+      const status = error.response?.status;
+
+      if (status === 409) {
+        return 'Conflict: 이미 존재하는 사용자입니다.';
+      }
+
+      if (status === 400) {
+        return 'Bad Request: 잘못된 소셜 로그인 타입입니다.';
+      }
+    }
+    return '알 수 없는 오류가 발생했습니다.';
   }
 };
