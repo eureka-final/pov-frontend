eea672118831789181a3cdc1f8f4774777b25909
shinhm1 <shinhm1@naver.com>
2024-12-04 01:46:03 +0900

    refactor: SignUp 페이지 내 고정 텍스트를 constant 변수로 수정

---
+++ src/constants/texts.ts
@@ -0,0 +1,22 @@
+export const SIGN_UP_HEADER_TEXTS = [
+  {
+    header: { firstLine: ['닉네임', '을'], secondLine: '입력해주세요' },
+    paragraph: '어떤 이름으로 불리고 싶으신가요?',
+  },
+  {
+    header: { firstLine: ['생년월일', '을'], secondLine: '입력해주세요' },
+    paragraph: '연령대에 맞는 영화 추천을 위해 사용돼요.',
+  },
+  {
+    header: { firstLine: ['관심있는 장르', '를'], secondLine: '선택해주세요' },
+    paragraph: '최대 3개까지 선택할 수 있어요.',
+  },
+  {
+    header: { firstLine: ['프로필 사진', '을'], secondLine: '등록해주세요' },
+    paragraph: '등록하지 않으면 기본 프로필이 지정돼요.',
+  },
+  {
+    header: { firstLine: ['회원정보', '를'], secondLine: '생성하고 있어요' },
+    paragraph: '잠시만 기다려주세요.',
+  },
+] as const;
