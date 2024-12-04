4e97856c8929ab94511787a9aed2d100c83e974b
kimdayeon37 <kdy37912@naver.com>
2024-12-04 10:48:43 +0900

    feat: toast store 생성

--- src/routes/AppPages.tsx
+++ src/routes/AppPages.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRouteDef } from './RouteDef';
 import Header from '../components/header/Header';
 import Padded from '../components/templates/Padded/Padded';
+import ToastContainer from '../components/common/ToastContainer/ToastContainer';
 
 const AppPages = () => {
   return (
     <BrowserRouter>
       <Padded>
         <Header />
+        <ToastContainer />
       </Padded>
       <Routes>
         {Object.entries({ ...AppRouteDef }).map(([name, { path, element }], index) => (
