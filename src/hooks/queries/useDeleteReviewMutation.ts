c127da58c2c1406c97785041088fe60bc4558070
kimdayeon37 <kdy37912@naver.com>
2024-12-04 11:26:08 +0900

    feat: error 처리 적용

--- src/hooks/queries/useDeleteReviewMutation.ts
+++ src/hooks/queries/useDeleteReviewMutation.ts
@@ -1,8 +1,17 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteReview } from '../../apis/review/deleteReview';
+import { ERROR_CODE } from '../../constants/api';
+import { useToast } from '../common/useToast';
+
+export interface ErrorResponseData {
+  statusCode?: number;
+  message?: string;
+  code?: number;
+}
 
 export const useDeleteReviewMutation = () => {
   const queryClient = useQueryClient();
+  const { createToast } = useToast();
 
   const deleteReviewMutation = useMutation({
     mutationFn: deleteReview,
@@ -10,9 +19,14 @@ export const useDeleteReviewMutation = () => {
       // 변이 성공 시 캐시 무효화로 리뷰 데이터 갱신
       queryClient.invalidateQueries({ queryKey: ['reviews'] });
     },
-    onError: (error) => {
+    onError: (error: ErrorResponseData) => {
       console.error('데이터 전송 실패:', error);
-      alert('리뷰 삭제에 실패했습니다.');
+      if (error.code && error.code > ERROR_CODE.TOKEN_ERROR_RANGE) {
+        createToast('인증 오류가 발생했습니다. 다시 로그인해주세요.');
+        return;
+      }
+
+      createToast('리뷰 삭제에 실패했습니다. 잠시 후 다시 시도해 주세요.');
     },
   }
   );
