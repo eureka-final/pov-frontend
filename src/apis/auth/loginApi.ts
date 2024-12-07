cc4beebd927b4161dbc6d04bd60851b765951c7c
shinhm1 <shinhm1@naver.com>
2024-12-07 13:17:11 +0900

    fix: 로그인, 회원가입 API 및 에러 핸들링 분리

--- src/apis/auth/loginApi.ts
+++ src/apis/auth/loginApi.ts
@@ -1,6 +1,4 @@
-import { useNavigate } from 'react-router-dom';
 import { instance } from '../axiosInstance';
-import axios from 'axios';
 
 /* 로그인 요청 API */
 export const postLoginApi = async (email: string, socialType: string) => {
@@ -11,24 +9,9 @@ export const postLoginApi = async (email: string, socialType: string) => {
     });
 
     console.log(response.data);
-    window.alert('로그인 성공');
-    window.location.href = '/main';
     return response.data;
   } catch (error) {
-    // TODO API - 라우터 이동 로직 분리 리팩토링 필요
-    if (axios.isAxiosError(error)) {
-      if (error.response?.status === 404) {
-        window.alert('회원가입으로 이동');
-        window.location.href = '/signup';
-        return;
-      }
-      if (error.response?.status === 400) {
-        window.alert('로그인 실패');
-        window.location.href = '/login';
-        return;
-      }
-    }
-    window.alert('로그인 실패');
-    window.location.href = '/login';
+    console.log(error);
+    throw error;
   }
 };
--- src/apis/auth/signupApi.ts
+++ src/apis/auth/signupApi.ts
@@ -8,16 +8,11 @@ export const postSignUpApi = async (user: User) => {
     const response = await instance.post('/api/auth/signup', user);
     return response.data;
   } catch (error) {
+    console.error(error);
+
     if (axios.isAxiosError(error)) {
       const status = error.response?.status;
-
-      if (status === 409) {
-        return 'Conflict: 이미 존재하는 사용자입니다.';
-      }
-
-      if (status === 400) {
-        return 'Bad Request: 잘못된 소셜 로그인 타입입니다.';
-      }
+      return status;
     }
     return '알 수 없는 오류가 발생했습니다.';
   }
