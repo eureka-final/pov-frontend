190c69390ab1a760c0b077119160da59c05577de
kimdayeon37 <kdy37912@naver.com>
2024-12-09 19:17:01 +0900

    style: 리뷰 상세 페이지 마크업

--- src/pages/Review/ReviewDetail/Index.tsx
+++ src/pages/Review/ReviewDetail/Index.tsx
@@ -5,22 +5,26 @@ import { Heading, Body, Paragraph, Icon, Badge } from 'pov-design-system';
 import { Container, HeaderContainer, Additionals, TitleInfo, ReviewInfo, Count, BodyContainer, BackgroundLayer } from './ReviewDetail.styles';
 import Profile from '../../../components/common/Profile';
 import { useDeleteReviewMutation } from '../../../hooks/queries/useDeleteReviewMutation';
+import dompurify from 'dompurify';
 
 const Index = () => {
   const [reviewData, setReviewData] = useState({
-    reviewer: '혜밍웨이',
-    profileImge: 'https://avatars2.githubusercontent.com/u/123456',
     title: '누구냐, 너',
-    date: '2003.11',
-    keywords: ['감동적인', '연출이 뛰어난'],
-    likes: '156',
     contents:
-      'Pizza ipsum dolor meat lovers buffalo. Lovers Bianca chicken buffalo beef thin large party Chicago pineapple. Ricotta pineapple thin pesto large anchovies. Hawaiian Bianca pepperoni large Aussie lot melted banana olives. Bianca meatball roll pork pepperoni lovers. Buffalo thin onions dolor Philly. Green party marinara black rib lot beef ham. Meatball dolor marinara onions string. Garlic Hawaiian sauce crust and. Pineapple anchovies thin Chicago bbq. Anchovies tomato peppers Aussie white mozzarella Aussie. Sauce Aussie marinara broccoli style marinara. Meat mozzarella sauce cheese ipsum dolor string. Lasagna meatball sautéed marinara beef pesto and. NY string party Chicago banana meat crust mouth. Dolor bell bell tomato cheese pie large. Extra string mouth sauce stuffed broccoli. Meatball burnt onions and pork personal tomatoes olives wing chicken. Buffalo personal thin style red ricotta Philly tossed. Thin ipsum pineapple roll party. Sautéed melted mouth Philly lot bacon pie. Tossed onions white meat sautéed extra Aussie tomato. Rib olives lot Philly sauce.',
+      '<p>이 영화는 정말 놀라웠습니다. 스토리 전개가 흥미진진하고 캐릭터들이 생동감 있게 느껴졌습니다.</p><img src="https://example.com/images/movie_scene.jpg" alt="영화 장면" />',
+    reviewer: '혜밍웨이',
+    profileImage: 'https://avatars2.githubusercontent.com/u/123456',
+    createdAt: '2024-12-09T10:10:19.360Z',
+    likeAmount: 10,
+    isLiked: true,
+    spoiler: true,
+    keywords: ['감동적인', '연출이 뛰어난'],
     url: 'https://s3-alpha-sig.figma.com/img/472e/ae15/f9f6158006f9a9a41457e6b4b6d6154e?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oTRHhPd9IUhqT~UvQ1cAGibItqIX~QP3qmoUvKRw45gi2Gf3cib6HeFLt32GBig2RDYo8xvUVz-Fg0jg78gwaWi6gl2GUmmhFHRGH-P7DW9cWCLDpCjku08nThf3L~-C-gOqL9CjS3322Drr9ZjtJk7GQZ2lMfZjnzF9RMXf~IzEGYOf-cRV-eFxe5GGhx0w2y~Fd32U7E8aIYODKefdq~GNMFVTb0Pr2Rkoi3bWr99Pr8oVEs4d-lvxrH8hn2M2uISXKd-1DBPQ1~yNp8RlNjtC-TlLuYWJN75XjnLJXJPagrjxVYGkgPxtVz5Co7t2CNrGyDB7BxRNf-EODwWw-A__',
   });
 
   const navigate = useNavigate();
   const { movieId, reviewId } = useParams<{ movieId: string; reviewId: string }>();
+  const sanitizer = dompurify.sanitize;
 
   const goToEditPage = () => {
     //navigate(`/${movieId}/review/edit/${reviewId}`);
@@ -45,13 +49,13 @@ const Index = () => {
             <Heading size="xLarge">{reviewData.title}</Heading>
           </TitleInfo>
           <ReviewInfo>
-            <Profile name={reviewData.reviewer} avatarUrl={reviewData.profileImge} />
+            <Profile name={reviewData.reviewer} avatarUrl={reviewData.profileImage} />
             <BodyContainer>
-              <Body size="large">{reviewData.date}</Body>
+              <Body size="large">{reviewData.createdAt}</Body>
             </BodyContainer>
             <Additionals>
               <Icon icon="heartfill" color="#0DE781" />
-              <Count color="#0DE781">{reviewData.likes}</Count>
+              <Count color="#0DE781">{reviewData.likeAmount}</Count>
             </Additionals>
             <Additionals>
               {reviewData.keywords.map((item, index) => (
@@ -64,7 +68,9 @@ const Index = () => {
         </HeaderContainer>
       </Container>
 
-      <Paragraph>{reviewData.contents}</Paragraph>
+      <Paragraph>
+        <div dangerouslySetInnerHTML={{ __html: sanitizer(`${reviewData.contents}`) }} />
+      </Paragraph>
       <button onClick={goToEditPage}>수정하기</button>
       <button onClick={handleDelete}>삭제하기</button>
     </Basic>
