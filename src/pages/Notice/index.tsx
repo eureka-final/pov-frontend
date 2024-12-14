337ac95e3a71e4c0bb8a0bee093989462e8c904d
shinhm1 <shinhm1@naver.com>
2024-12-14 12:04:49 +0900

    feat: 알림 읽음 처리 구현

--- src/pages/Notice/index.tsx
+++ src/pages/Notice/index.tsx
@@ -2,15 +2,18 @@ import Padded from '../../components/templates/Padded/Padded';
 import { Heading, Body } from 'pov-design-system';
 import { AlarmCardWrapper, AlarmCardContainer, NoContentsContainer, TimeText } from './index.style';
 import { useNoticesQuery } from '../../hooks/queries/useNoticesQuery';
+import { putNoticeRead } from '../../apis/notice/putNotice';
+import { useNavigate } from 'react-router-dom';
 
 interface AlarmCardProps {
   content: string;
   createdAt: string;
+  onClick: () => Promise<void>;
 }
 
-const AlarmCard = ({ content, createdAt }: AlarmCardProps) => {
+const AlarmCard = ({ content, createdAt, onClick }: AlarmCardProps) => {
   return (
-    <AlarmCardContainer>
+    <AlarmCardContainer onClick={onClick}>
       <Body size="large">{content}</Body>
       <TimeText>
         <Body size="medium">{createdAt}</Body>
@@ -21,6 +24,16 @@ const AlarmCard = ({ content, createdAt }: AlarmCardProps) => {
 
 const index = () => {
   const { noticesData } = useNoticesQuery();
+  const navigate = useNavigate();
+
+  // const handleClickNotice = async (noticeId: string, movieId: string, reviewId: string) => {
+  //   const response = await putNoticeRead(noticeId);
+
+  //   // TODO 404일 경우 존재하지 않는 알림입니다 toast 노출
+  //   if (response) {
+  //     navigate(`/review/${movieId}/detail/${reviewId}`);
+  //   }
+  // };
 
   return (
     <Padded>
@@ -33,7 +46,10 @@ const index = () => {
             <Body size="large">알림이 없어요.</Body>
           </NoContentsContainer>
         )}
-        {noticesData && noticesData.data.map((data, index) => <AlarmCard key={index} content={data.content} createdAt={data.createdAt} />)}
+        {noticesData &&
+          noticesData.data.map((data, index) => (
+            <AlarmCard key={index} content={data.content} createdAt={data.createdAt} onClick={() => putNoticeRead(data.id)} />
+          ))}
       </AlarmCardWrapper>
     </Padded>
   );
