0d9d5e0ed99374e80738413af8f4b03915f36ac0
shinhm1 <shinhm1@naver.com>
2024-12-16 04:00:36 +0900

    feat: usePremiereEntryQuery 구현

---
+++ src/hooks/queries/usePremieresEntryQuery.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPremieresEntry } from '../../apis/premieres/getPremieres';
+import { PremiereEntryResponse } from '../../types/premiere';
+
+export const usePremieresEntryQuery = () => {
+  const { data: premiereEntryData } = useQuery<PremiereEntryResponse>({
+    queryKey: ['premieresEntry'],
+    queryFn: getPremieresEntry,
+  });
+
+  return { premiereEntryData };
+};
