d9caac9b625c9c6607ed195bec5b47f5322d4716
kimdayeon37 <kdy37912@naver.com>
2024-12-17 02:35:46 +0900

    fix: 스켈레톤 컴포넌트

--- src/pages/Movie/MoviePageSkeleton.tsx
+++ src/pages/Movie/MoviePageSkeleton.tsx
@@ -1,24 +1,21 @@
 import { Skeleton } from 'pov-design-system';
 import { Container } from './Movie.styles';
-import Padded from '../../components/templates/Padded/Padded';
 import { SectionContainer, CardContainer } from '../../components/movies/Section/Section.styles';
 import { CardWapper } from '../../components/movies/Card/Card.styles';
 
 const MoviePageSkeleton = () => {
   return (
     <>
-      <Padded>
-        <Container>
-          <SectionContainer>
-            <CardContainer>
-              <CardWapper>
-                <Skeleton width="156px" height="233px" />
-                <Skeleton width="100%" height="60px" />
-              </CardWapper>
-            </CardContainer>
-          </SectionContainer>
-        </Container>
-      </Padded>
+      <Container>
+        <SectionContainer>
+          <CardContainer>
+            <CardWapper>
+              <Skeleton width="156px" height="233px" />
+              <Skeleton width="156px" height="60px" />
+            </CardWapper>
+          </CardContainer>
+        </SectionContainer>
+      </Container>
     </>
   );
 };
