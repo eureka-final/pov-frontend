340936250b22ea0aad31e5472b1fbbf9f95a1cbd
shinhm1 <shinhm1@naver.com>
2024-12-16 13:03:03 +0900

    fix: prod-ci .env setting 단계 추가

--- src/utils/firebase/initFirebase.ts
+++ src/utils/firebase/initFirebase.ts
@@ -3,9 +3,9 @@ import { getMessaging } from 'firebase/messaging';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  authDomain: 'final-pov.firebaseapp.com',
+  projectId: 'final-pov',
+  storageBucket: 'final-pov.firebasestorage.app',
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
