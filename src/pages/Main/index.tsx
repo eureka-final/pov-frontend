77e65e9af247f05a1c25ef64c4e2e0b29a08304c
kimdayeon37 <kdy37912@naver.com>
2024-12-16 16:24:46 +0900

    feat: 메인 페이지 영화 컴포넌트 6개 가져오기

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,17 +3,32 @@ import { Container } from '../Movie/Movie.styles';
 import ReviewClubCard from '../../components/review/ReviewClubCard';
 import { ClubReviewListContainer } from '../../components/review/ReviewCard.style';
 import { useReviewsQuery } from '../../hooks/queries/useReviewsQuery';
+import { useMoviesQuery } from '../../hooks/queries/useMoviesQuery';
 import { Heading, ShowMoreBtn } from 'pov-design-system';
 import { useNavigate } from 'react-router-dom';
 import { SectionWrapper } from '../../components/club/ClubDetail/ClubDetail.styles';
+import { CardContainer } from '../../components/movies/Section/Section.styles';
+import Card from '../../components/movies/Card/Card';
 
 const Index = () => {
-  const { reviewsData } = useReviewsQuery();
   const navigate = useNavigate();
+
+  const { moviesData } = useMoviesQuery();
+  const { reviewsData } = useReviewsQuery();
 
   return (
     <Padded>
       <Container>
+        <SectionWrapper>
+          <Heading size="large">이런 영화 어때요? 🎞️</Heading>
+          <ShowMoreBtn onClick={() => navigate(`/movie`)} />
+        </SectionWrapper>
+        <CardContainer>
+          {moviesData.slice(0, 6).map((item, index) => (
+            <Card key={item.title + index} item={item} />
+          ))}
+        </CardContainer>
+
         <SectionWrapper>
           <Heading size="large">지금 인기있는 리뷰 💬</Heading>
           <ShowMoreBtn onClick={() => navigate(`/review`)} />
