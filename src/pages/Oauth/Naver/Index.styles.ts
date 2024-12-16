b940f597ad0de39399dc9784542789a377661206
shinhm1 <shinhm1@naver.com>
2024-12-16 11:55:48 +0900

    fix: 빌드 오류 수정

---
+++ src/pages/Oauth/Naver/Index.styles.ts
@@ -0,0 +1,11 @@
+/* section, wrapper */
+import styled from '@emotion/styled';
+
+export const LoadingSection = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
