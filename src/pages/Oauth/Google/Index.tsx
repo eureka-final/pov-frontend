652a0a9773042bef22f42fff0598aae1faa7233c
shinhm1 <shinhm1@naver.com>
2024-12-18 03:48:37 +0900

    fix: 네이버, 구글 로그인 및 회원가입 로직 및 Access Token 저장소 변경

--- src/pages/Oauth/Google/Index.tsx
+++ src/pages/Oauth/Google/Index.tsx
@@ -14,41 +14,50 @@ const Index = () => {
   useEffect(() => {
     const loginWithGoogle = async () => {
       const params = new URLSearchParams(location.search);
-
       const code = params.get('code');
-
       navigate(location.pathname, { replace: true }); // URL에서 쿼리 파라미터 제거
 
-      if (code) {
-        const data = await getGoogleUserInfoApi(code);
-        console.log(data);
+      // 인가 코드가 존재하지 않는 경우 에러 처리
+      if (!code) {
+        alert('서비스의 문제로 인해 로그인에 실패했어요.');
+        window.location.href = '/';
+        return;
+      }
 
-        if (data) {
-          try {
-            const response = await postLoginApi(data.email, 'GOOGLE');
+      const data = await getGoogleUserInfoApi(code);
+      // 구글에서 회원정보를 받아오는 데 실패한 경우 에러 처리
+      if (!data) {
+        alert('구글에서 회원정보를 받아오는 데 실패했어요.');
+        window.location.href = '/';
+        return;
+      }
 
-            if (response.data.exists) {
-              setLoggedIn(true);
-              setUser(response.data.memberInfo);
-              alert('로그인 성공');
-              navigate('/main');
-            } else {
-              alert('최초 로그인, 회원가입으로 이동');
-              navigate('/signup', {
-                state: {
-                  email: data.email,
-                  profileImage: data.profileImage,
-                  socialType: 'GOOGLE',
-                },
-              });
-            }
-          } catch (error) {
-            console.error(error);
-            alert('로그인 실패');
-            window.location.href = '/login';
-            return;
-          }
+      // 서버로 로그인 요청
+      try {
+        const response = await postLoginApi(data.email, 'GOOGLE');
+
+        if (response.data.exists) {
+          // 회원 정보가 이미 존재하는 경우 홈으로 이동
+          setLoggedIn(true);
+          setUser(response.data.memberInfo);
+          alert('로그인 성공');
+          // window.location.href = '/';
+        } else {
+          // 회원 정보가 존재하지 않는 경우 회원가입으로 이동
+          alert('최초 로그인, 회원가입으로 이동');
+          navigate('/signup', {
+            state: {
+              email: data.email,
+              profileImage: data.profileImage,
+              socialType: 'GOOGLE',
+            },
+          });
         }
+      } catch (error) {
+        console.error(error);
+        alert('로그인 실패');
+        // window.location.href = '/login';
+        return;
       }
     };
     loginWithGoogle();
--- src/pages/Oauth/Naver/Index.tsx
+++ src/pages/Oauth/Naver/Index.tsx
@@ -18,38 +18,39 @@ const Index = () => {
       const params = new URLSearchParams(location.search);
 
       const email = params.get('email');
-      const exists = params.get('exists');
-      const image = params.get('profileImage');
+      const profileImage = params.get('image');
 
       navigate(location.pathname, { replace: true }); // URL에서 쿼리 파라미터 제거
 
-      if (exists && email) {
-        const response = await postLogin(email, 'NAVER');
+      // 쿼리 파라미터에 이메일 정보가 없는 경우 에러 처리
+      if (!email) {
+        alert('네이버에서 사용자 정보를 받아오는 데 실패했어요.');
+        window.location.href = '/login';
+        return;
+      }
 
-        if (response) {
-          setLoggedIn(true);
-          setUser(response.data.memberInfo);
-          alert('로그인 완료');
-          window.location.href = '/';
-        } else {
-          alert('로그인 실패');
-          window.location.href = '/';
-        }
-      } else if (email) {
-        alert('사용자 정보 없음, 회원가입 필요');
-        navigate('/signup', {
-          state: {
-            email: email,
-            profileImage: image,
-            socialType: 'NAVER',
-          },
-        });
-      } else {
-        alert('콜백 URL에 이메일 정보가 없습니다. 처음부터 다시 시도해주세요.');
+      // 쿼리 파라미터 정보로 서버 로그인 API 호출
+      const response = await postLogin(email, 'NAVER');
+      // 서버 측 응답이 잘못된 경우 에러 처리
+      if (!response) {
+        alert('로그인에 실패했어요. 다시 시도해주세요.');
         window.location.href = '/login';
       }
-    };
 
+      if (response.data.exists) {
+        // 회원 정보가 존재하는 경우 로그인 성공, 메인 페이지로 이동
+        console.log(response);
+        setLoggedIn(true);
+        setUser(response.data.memberInfo);
+        alert('로그인 완료');
+        // window.location.href = '/';
+      } else {
+        // 회원 정보가 존재하지 않는 경우 회원가입 페이지로 이동
+        alert('최초 로그인, 회원가입 페이지로 이동');
+        navigate('/signup', { state: { email: email, profileImage: profileImage, socialType: 'NAVER' } });
+        window.location.href = '/';
+      }
+    };
     loginWithNaver();
   }, []);
 
