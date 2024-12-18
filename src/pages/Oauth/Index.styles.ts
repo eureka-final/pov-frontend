ec6101507c4ded8998707a1c61a8057aaa70e411
shinhm1 <shinhm1@naver.com>
2024-12-18 14:26:31 +0900

    feat: 구글, 네이버 로그인 페이지 프로그레스 애니메이션 및 모달 적용

--- src/pages/Oauth/Index.styles.ts
+++ src/pages/Oauth/Index.styles.ts
@@ -3,9 +3,25 @@ import styled from '@emotion/styled';
 
 export const LoadingSection = styled.section`
   width: 100%;
-  height: 100%;
+  height: calc(100svh - 160px);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 20px;
+  margin: 12px;
+`;
+
+export const ModalBodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 4px;
+  color: ${({ theme }) => theme.teritary};
 `;
