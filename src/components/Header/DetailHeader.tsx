9cbc5abc43cd211defd4bc6c9255b9b00b0c0f6c
shinhm1 <shinhm1@naver.com>
2024-12-18 22:15:08 +0900

    style: Header 내 Profile 컴포넌트 수정 및 중복 Padding 제거

--- src/components/Header/DetailHeader.tsx
+++ src/components/Header/DetailHeader.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from '@emotion/react';
 import { DetailHeaderWrapper, IconContainer } from './DetailHeader.style';
 import { Icon, Body } from 'pov-design-system';
+import Padded from '../templates/Padded/Padded';
 
 interface DetailHeaderProps {
   headerTitle: string;
