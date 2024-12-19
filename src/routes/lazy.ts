5c6895dd86695f2c21775119ef8fce65ad2af4d6
shinhm1 <shinhm1@naver.com>
2024-12-19 20:24:47 +0900

    fix: build 오류 수정

--- src/routes/lazy.ts
+++ src/routes/lazy.ts
@@ -1,23 +1,13 @@
 import { lazy } from 'react';
 
-export const MainPage = lazy(
-  () => import('../pages/Main')
-);
+export const MainPage = lazy(() => import('../pages/Main'));
 
-export const MoviePage = lazy(
-  () => import('../pages/Movie/Index')
-);
+export const MoviePage = lazy(() => import('../pages/Movie/index'));
 
-export const LogInPage = lazy(
-  () => import('../pages/Login/Index')
-);
+export const LogInPage = lazy(() => import('../pages/Login/Index'));
 
 export const MyPage = lazy(() => import('../pages/MyPage'));
 
-export const ClubPage = lazy(
-  () => import('../pages/Club/')
-);
+export const ClubPage = lazy(() => import('../pages/Club/'));
 
-export const PremieresPage = lazy(
-  () => import('../pages/Premieres')
-);
+export const PremieresPage = lazy(() => import('../pages/Premieres'));
