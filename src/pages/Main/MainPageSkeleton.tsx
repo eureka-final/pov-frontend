d661f003d501ad1f30bb3b368101985f356669ff
kimdayeon37 <kdy37912@naver.com>
2024-12-19 12:10:23 +0900

    feat: 메인페이지 스켈레톤 추가

--- src/pages/Main/MainPageSkeleton.tsx
+++ src/pages/Main/MainPageSkeleton.tsx
@@ -2,6 +2,8 @@ import { Skeleton } from 'pov-design-system';
 import { SectionContainer, CardContainer } from '../../components/movies/Section/Section.styles';
 import { MarginTop } from '../../components/movies/Card/Card.styles';
 import MoviePageSkeleton from '../Movie/MoviePageSkeleton';
+import { PopularReviewListContainer } from '../../components/review/ReviewCard.style';
+import ReviewPageSkeleton from '../../components/review/ReviewPageSkeleton';
 
 const MainPageSkeleton = () => {
   return (
@@ -12,6 +14,13 @@ const MainPageSkeleton = () => {
           <MoviePageSkeleton />
         </CardContainer>
       </SectionContainer>
+
+      <Skeleton width="100%" height="30px" />
+      <PopularReviewListContainer>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <ReviewPageSkeleton key={`initial-skeleton-${index}`} />
+        ))}
+      </PopularReviewListContainer>
     </MarginTop>
   );
 };
