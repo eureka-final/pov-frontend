7446f6083fbda1017f989fb796548a67034b266f
hmin___ <44727850+shinhm1@users.noreply.github.com>
2024-12-19 12:47:30 +0900

    Revert "Pov lazy suspense"

--- src/pages/Movie/MoviePageSkeleton.tsx
+++ src/pages/Movie/MoviePageSkeleton.tsx
@@ -1,22 +1,26 @@
 import { Skeleton } from 'pov-design-system';
-import { CardContainer } from '../../components/movies/Section/Section.styles';
-import { CardWapper } from '../../components/movies/Card/Card.styles';
+import { SectionContainer, CardContainer } from '../../components/movies/Section/Section.styles';
+import { CardWapper, MarginTop } from '../../components/movies/Card/Card.styles';
 import useWindowSize from '../../hooks/utils/useWindowSize';
 
 const MoviePageSkeleton = () => {
   const windowSize = useWindowSize();
 
   return (
-    <CardContainer>
-      {Array.from({ length: 6 }).map((_, index) => (
-        <CardWapper key={`skeleton-${index}`}>
-          <Skeleton width={windowSize.width! > 600 ? '169px' : '169px'} height={windowSize.width! > 600 ? '282px' : '282px'} />
-          <Skeleton width={windowSize.width! > 600 ? '100px' : '169px'} height="20px" />
-          <Skeleton width={windowSize.width! > 600 ? '40px' : '169px'} height="16px" />
-          <Skeleton width={windowSize.width! > 600 ? '60px' : '169px'} height="20px" />
-        </CardWapper>
-      ))}
-    </CardContainer>
+    <MarginTop>
+      <SectionContainer>
+        <CardContainer>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <CardWapper key={`skeleton-${index}`}>
+              <Skeleton width={windowSize.width! > 600 ? '169px' : '169px'} height={windowSize.width! > 600 ? '282px' : '282px'} />
+              <Skeleton width={windowSize.width! > 600 ? '100px' : '169px'} height="20px" />
+              <Skeleton width={windowSize.width! > 600 ? '40px' : '169px'} height="16px" />
+              <Skeleton width={windowSize.width! > 600 ? '60px' : '169px'} height="20px" />
+            </CardWapper>
+          ))}
+        </CardContainer>
+      </SectionContainer>
+    </MarginTop>
   );
 };
 
