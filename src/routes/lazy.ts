036b6d0bd489660dd30c3ada340ab1f91b77f620
kimdayeon37 <kdy37912@naver.com>
2024-12-23 13:32:32 +0900

    refactor: Index

--- src/routes/lazy.ts
+++ src/routes/lazy.ts
@@ -1,13 +1,3 @@
 import { lazy } from 'react';
 
-export const MainPage = lazy(() => import('../pages/Main'));
-
-export const MoviePage = lazy(() => import('../pages/Movie/index'));
-
-export const LogInPage = lazy(() => import('../pages/Login/Index'));
-
-export const MyPage = lazy(() => import('../pages/MyPage'));
-
-export const ClubPage = lazy(() => import('../pages/Club/'));
-
-export const PremieresPage = lazy(() => import('../pages/Premieres'));
+export const MainPage = lazy(() => import('../pages/Main/Index'));
